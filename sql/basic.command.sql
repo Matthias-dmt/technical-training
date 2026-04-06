dd97483c3d448a8547b71b669406340569d07462
Matthias <matthiasdumont.pro@gmail.com>
2026-04-06 14:57:34 +0200

    sql scenarios update and insert

--- sql/basic.command.sql
+++ sql/basic.command.sql
@@ -1,4 +1,4 @@
-// example idempotency
+-- // example idempotency
 
 CREATE TABLE webhook_events (
   id SERIAL PRIMARY KEY,
@@ -13,7 +13,7 @@ ON CONFLICT (event_id) DO UPDATE SET status = EXCLUDED.status
 RETURNING id;
 
 
-// Zero downtime migrations
+-- // Zero downtime migrations
 
 const batchSize = 1000;
 const lastId = 0;
@@ -36,3 +36,39 @@ while(true) {
     await sleep(100)
 }
 
+-- // SCENARIOS
+
+-- Scenario A:
+-- A user clicks "Place Order".
+-- You need to check if the product still has stock (quantity > 0),
+-- decrement it by 1,
+-- and create the order
+-- all without two users buying the last item simultaneously.
+
+-- Wrap around transaction
+-- use select for update to lock to touched row
+BEGIN;
+
+SELECT *
+FROM products
+WHERE id = $1 AND quantity > 0
+FOR UPDATE;
+
+UPDATE products
+SET quantity = quantity - 1
+WHERE id = $1;
+
+INSERT INTO orders (user_id, product_id)
+VALUES ($2, $1)
+
+COMMIT;
+
+-- Scenario B:
+-- An analytics service receives events from multiple sources.
+-- Each event has a unique event_id.
+-- You want to process each event exactly once and skip duplicates.
+
+INSERT INTO events (event_id)
+VALUES ('event_123')
+ON CONFLICT (event_id) DO NOTHING
+RETURN id;
